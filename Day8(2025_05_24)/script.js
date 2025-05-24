9df821c1f804078412f658bdc6b95e084127d1cc
Kun <longdh.udu.ptit@gmail.com>
2025-05-24 22:48:24 +0700

    update

---
+++ Day8(2025_05_24)/script.js
@@ -0,0 +1,50 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.buttons button');
+
+let currentInput = '';
+let operator = '';
+let operand1 = '';
+let operand2 = '';
+
+buttons.forEach(button => {
+    button.addEventListener('click', () =>{
+        const value = button.textContent;
+
+        if (!isNaN(value)) {
+            currentInput += value;
+            display.value = currentInput;
+        } else if (value === 'C') {
+            currentInput = '';
+            operand1 = '';
+            operand2 = '';
+            operator = '';
+            display.value = '';
+        } else if (value === '=') {
+            if (operand1 && operator && currentInput) {
+                operand2 = currentInput;
+                let result;
+
+                switch (operator) {
+                    case '+' : result = parseFloat(operand1) + parseFloat(operand2); break ;
+                    case '-' : result = parseFloat(operand1) - parseFloat(operand2); break ;
+                    case 'x' : result = parseFloat(operand1) * parseFloat(operand2); break ;
+                    case '/' :
+                        result = (parseFloat(operand2) === '0') ? 'Error' : parseFloat(operand1) / parseFloat(operand2); break;  
+                }
+
+
+                display.value = result;
+                currentInput = result.toString();
+                operand1 = '';
+                operand2 = '';
+                operator = '';
+            }
+        }else {
+            if (currentInput) {
+                operand1 = currentInput;
+                operator = value;
+                currentInput = '';
+            }
+        }
+    })
+})
